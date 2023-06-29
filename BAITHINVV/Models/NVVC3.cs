4a01aa9db95b3a0274f12dd718793833e0ce7f4b
Vuong702 <vuong180900@gmail.com>
2023-06-29 09:30:57 +0700

    first commit

---
+++ BAITHINVV/Models/NVVC3.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BAITHINVV.Models
+{
+    public class NVVC3
+    {
+        public string NVVTEN { get;set; }
+          public string NVVQUAN { get;set;}
+           public string TUOI { get;set;}
+    }
+
+}
